a2b61d6e0a49a2e460b520a9c4c0da16a637d693
SUBASRI <sukusuba2@gmail.com>
2025-03-16 17:58:36 +0530

    Delete nostra directory

--- nostra/contact.js
+++
@@ -1,9 +0,0 @@
-var menuicon=document.getElementById("menuicon")
-var sidenav = document.getElementById("sidenav")
-menuicon.addEventListener("click",function(){
-    sidenav.style.marginLeft="50%"
-})
-
-document.getElementById("closenav").addEventListener("click",()=>{
-    sidenav.style.marginLeft = "-50%"
-})
